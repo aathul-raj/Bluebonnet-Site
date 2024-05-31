e0ae091a260a2b5ca0fa43b97d02e69e48907057
athul <athulrsuresh@gmail.com>
2024-05-30 22:32:36 -0500

    favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
